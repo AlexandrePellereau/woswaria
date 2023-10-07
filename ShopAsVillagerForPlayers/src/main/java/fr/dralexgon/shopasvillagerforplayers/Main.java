75cdb8af6f27b727de349c2d15e769baa93d26cb
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2023-10-07 22:48:06 +0200

    Rename ShopPlayerPNJ by ShopAsVillagerForPlayer and change project to intellij idea from eclipse

---
+++ ShopAsVillagerForPlayers/src/main/java/fr/dralexgon/shopasvillagerforplayers/Main.java
@@ -0,0 +1,129 @@
+package fr.dralexgon.shopasvillagerforplayers;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Villager;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.dralexgon.shopasvillagerforplayers.commands.CommandGiveSkinVillagerShop;
+import fr.dralexgon.shopasvillagerforplayers.commands.Commands;
+import fr.dralexgon.shopasvillagerforplayers.commands.TabCompletionSkinVillagerShop;
+import fr.dralexgon.shopasvillagerforplayers.gui.Gui;
+import fr.dralexgon.shopasvillagerforplayers.gui.ListenersGUI;
+import fr.dralexgon.shopasvillagerforplayers.saveandload.SaveAndLoad;
+import fr.dralexgon.shopasvillagerforplayers.saveandload.UseCsvFiles;
+
+import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+
+public class Main extends JavaPlugin {
+	
+	private List<VillagerShop> listVillagersShop;
+	private List<VillagerShop> listVillagersShopInactive;
+	private Gui gui;
+	private UseCsvFiles useCsvFiles;
+	private List<List> tempVariables;
+	private long expirationTime;
+	private static Main instance;
+	
+	@Override
+	public void onEnable() {
+		saveDefaultConfig();
+		Main.instance = this;
+		
+		this.expirationTime = 1000*60*60*24*365;
+		this.listVillagersShop = new ArrayList<VillagerShop>();
+		this.listVillagersShopInactive = new ArrayList<VillagerShop>();
+		this.tempVariables = new ArrayList<List>();
+		this.useCsvFiles = new UseCsvFiles();
+		this.gui = new Gui(this);
+		
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvents(new Listeners(this), this);
+		pm.registerEvents(new ListenersGUI(this), this);
+		getCommand("giveskinvillagershop").setExecutor(new CommandGiveSkinVillagerShop());
+		getCommand("giveskinvillagershop").setTabCompleter(new TabCompletionSkinVillagerShop());
+		getCommand("givevillagershop").setExecutor(new Commands());
+		getCommand("givevillagershopinfinitetrade").setExecutor(new Commands());
+		getCommand("givevillagershopkiller").setExecutor(new Commands());
+		
+		SaveAndLoad.load();
+		
+		System.out.println("[ShopPlayerPNJ-FDA] Enabled !");
+	}
+	
+	public static Main getInstance() {
+		return Main.instance;
+	}
+	
+	public List<VillagerShop> getListVillagersShop() {
+		return this.listVillagersShop;
+	}
+	
+	public List<VillagerShop> getListVillagersShopInactive() {
+		return this.listVillagersShopInactive;
+	}
+	
+	public Gui getGui() {
+		return this.gui;
+	}
+	
+	public UseCsvFiles getUseCsvFiles() {
+		return this.useCsvFiles;
+	}
+	
+	public List<List> getTempVariables() {
+		return this.tempVariables;
+	}
+	
+	public long getExpirationTime() {
+		return this.expirationTime;
+	}
+	
+	public boolean removeInventory(Inventory inventory, Material type, int nb) {
+		int nbMissing = nb;
+		for (ItemStack itemStack : inventory.getContents()) {
+			if (itemStack!=null) {
+				if (itemStack.getType()==type) {
+					if (itemStack.getAmount()>=nbMissing) {
+						itemStack.setAmount(itemStack.getAmount()-nb);
+						return true;
+					}else {
+						nbMissing-=itemStack.getAmount();
+						itemStack.setAmount(0);
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	public void addTrade(Villager villager, ItemStack inputTrade, ItemStack inputTrade2, ItemStack outputTrade) {
+		List<MerchantRecipe> tempListOfRecipes = new ArrayList<MerchantRecipe>();
+		for (MerchantRecipe recipe : villager.getRecipes()) {
+			tempListOfRecipes.add(recipe);
+		}
+		MerchantRecipe newTrade = new MerchantRecipe(outputTrade.clone(),0);
+		newTrade.addIngredient(inputTrade.clone());
+		if (inputTrade2 != null) {
+			newTrade.addIngredient(inputTrade2.clone());
+		}
+		newTrade.setExperienceReward(false);
+		tempListOfRecipes.add(newTrade);
+		villager.setRecipes(tempListOfRecipes);
+	}
+	
+	
+	
+	@Override
+	public void onDisable() {
+		SaveAndLoad.save();
+		System.out.println("[ShopPlayerPNJ-FDA] Disabled !");
+	}
+}
---
+++ ShopAsVillagerForPlayers/src/main/java/fr/dralexgon/shopasvillagerforplayers/commands/TabCompletionSkinVillagerShop.java
@@ -0,0 +1,20 @@
+package fr.dralexgon.shopasvillagerforplayers.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Villager.Profession;
+
+public class TabCompletionSkinVillagerShop implements TabCompleter{
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command cmd, String arg2, String[] arg3) {
+		List<String> listStringProfession = new ArrayList<String>();
+		for (Profession profession : Profession.values()) {
+			listStringProfession.add(profession+"");
+		}
+		return listStringProfession;
+	}
+}
