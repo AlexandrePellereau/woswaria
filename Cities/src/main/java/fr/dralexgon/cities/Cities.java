c7beb3108e6d0e6ce497663c8c2901e14a88c05a
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2023-12-10 21:49:57 +0100

    prepating multiple language support

--- Cities/src/main/java/fr/dralexgon/cities/Cities.java
+++ Cities/src/main/java/fr/dralexgon/cities/Cities.java
@@ -2,16 +2,24 @@ package fr.dralexgon.cities;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public final class Cities extends JavaPlugin {
+
+    static Locale locale;
+    static ResourceBundle bundle;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Cities.locale = new Locale("fr", "FR");
+        Cities.bundle = ResourceBundle.getBundle("Bundle", Cities.locale);
+        System.out.println("Hello SpigotMC");
+        System.out.println(Cities.bundle.getString("console.plugin_enable"));
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        System.out.println("Cities plugin is disabled");
     }
 }
